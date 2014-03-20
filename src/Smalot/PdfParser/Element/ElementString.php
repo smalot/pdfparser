bee10f9a7c2e272644b734ac3ae3f5157539b79a
Daniel Ruf <DanielRuf@users.noreply.github.com>
2014-03-20 22:03:50 +0100

    Fix #26
    only the last occurrence of ) has to be escaped and not the first occurrence

--- src/Smalot/PdfParser/Element/ElementString.php
+++ src/Smalot/PdfParser/Element/ElementString.php
@@ -74,7 +74,7 @@ class ElementString extends Element
 
             // Find next ')' not escaped.
             $cur_start_text = $start_search_end = 0;
-            while (($cur_start_pos = strpos($name, ')', $start_search_end)) !== false) {
+            while (($cur_start_pos = strrpos($name, ')', $start_search_end)) !== false) {
                 $cur_extract = substr($name, $cur_start_text, $cur_start_pos - $cur_start_text);
                 preg_match('/(?P<escape>[\\\]*)$/s', $cur_extract, $match);
                 if (!(strlen($match['escape']) % 2)) {
