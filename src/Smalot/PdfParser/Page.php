9896517f22c7311eaffb64f6741c49a520a53d10
Dave Lawrence <david@sitesolve.com>
2014-10-29 12:43:53 -0600

    add getTextArray methods to Page.php and Object.php to enable retrieving text in separate array elements rather than one long string

--- src/Smalot/PdfParser/Page.php
+++ src/Smalot/PdfParser/Page.php
@@ -219,4 +219,50 @@ class Page extends Object
 
         return '';
     }
+
+	/**
+	 * @param Page
+	 *
+	 * @return array
+	 */
+	public function getTextArray(Page $page = null)
+	{
+		if ($contents = $this->get('Contents')) {
+
+			if ($contents instanceof ElementMissing) {
+				return array();
+			} elseif ($contents instanceof Object) {
+				$elements = $contents->getHeader()->getElements();
+
+				if (is_numeric(key($elements))) {
+					$new_content = '';
+
+					foreach ($elements as $element) {
+						if ($element instanceof ElementXRef) {
+							$new_content .= $element->getObject()->getContent();
+						} else {
+							$new_content .= $element->getContent();
+						}
+					}
+
+					$header   = new Header(array(), $this->document);
+					$contents = new Object($this->document, $header, $new_content);
+				}
+			} elseif ($contents instanceof ElementArray) {
+				// Create a virtual global content.
+				$new_content = '';
+
+				foreach ($contents->getContent() as $content) {
+					$new_content .= $content->getContent() . "\n";
+				}
+
+				$header   = new Header(array(), $this->document);
+				$contents = new Object($this->document, $header, $new_content);
+			}
+
+			return $contents->getTextArray($this);
+		}
+
+		return array();
+	}
 }
