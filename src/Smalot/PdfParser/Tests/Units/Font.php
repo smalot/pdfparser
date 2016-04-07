a2d0c23d77328160bdfbc9ab723729362850cb39
sparx82 <sparx23@gmx.net>
2016-04-07 08:05:09 +0200

    Issue95

--- src/Smalot/PdfParser/Tests/Units/Font.php
+++ src/Smalot/PdfParser/Tests/Units/Font.php
@@ -295,5 +295,30 @@ end';
             ),
         );
         $this->assert->string($font->decodeText($commands))->isEqualTo('Docu Docu');
+        
+        //Check if ANSI/Unicode detection and conversion is working properly
+        $filename = __DIR__ . '/../../../../../samples/bugs/Issue95_ANSI.pdf';
+        $parser   = new \Smalot\PdfParser\Parser();
+        $document = $parser->parseFile($filename);
+        $fonts    = $document->getFonts();
+        /** @var \Smalot\PdfParser\Font $font */
+        $font     = reset($fonts);
+        $commands = array(
+            array(
+                't' => '<',
+                'c' => "E6F6FC", //ANSI encoded string
+            ),
+        );
+        echo("----------------------------------------------------------------\n");
+        $this->assert->string($font->decodeText($commands))->isEqualTo('æöü');    
+        
+        $commands = array(
+            array(
+                't' => '<',
+                'c' => "C3A6C3B6C3BC", //Unicode encoded string
+            ),
+        );
+        echo("----------------------------------------------------------------\n");
+        $this->assert->string($font->decodeText($commands))->isEqualTo('æöü');
     }
 }
