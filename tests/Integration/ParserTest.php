56cb6b0c07002ab3bf152ab3cf66a4867ee07b03
PaulBehrendtVentoro <paul.behrendt@ventoro.de>
2020-07-15 13:12:08 +0200

    fix missing catalog bug (+ some code refactoring) (#312)
    * fix missing catalog bug
    * added some assertions
    * Update tests/Integration/ParserTest.php according to suggestion
    * ignore whitespace characters at $offset in RawDataParser::getIndirectObject
    * some refactoring in RawDataParser.php
    * update to comments
    
    Co-authored-by: paulbehrendtventoro <paul.behrendt@deutsche-fensterbau.de>
    Co-authored-by: Jérémy Benoist <j0k3r@users.noreply.github.com>

--- tests/Integration/ParserTest.php
+++ tests/Integration/ParserTest.php
@@ -34,6 +34,7 @@ namespace Tests\Smalot\PdfParser\Integration;
 
 use Exception;
 use Smalot\PdfParser\Parser;
+use Smalot\PdfParser\XObject\Image;
 use Test\Smalot\PdfParser\TestCase;
 
 class ParserTest extends TestCase
@@ -77,26 +78,17 @@ class ParserTest extends TestCase
     }
 
     /**
-     * Parsing certain PDFs may lead to following notices:
-     *
-     *      Notice: Trying to access array offset on value of type int
-     *
-     * and to an exception:
-     *
-     *      Missing catalog.
+     * Test that issue related pdf can now be parsed
      *
      * @see https://github.com/smalot/pdfparser/issues/267
      */
     public function testIssue267()
     {
-        $this->expectException(Exception::class);
-        $this->expectExceptionMessage('Missing catalog.');
-
         $filename = $this->rootDir.'/samples/bugs/Issue267_array_access_on_int.pdf';
 
         $document = $this->fixture->parseFile($filename);
 
-        // triggers the exception
-        $document->getPages();
+        $this->assertEquals(Image::class, \get_class($document->getObjectById('128_0')));
+        $this->assertStringContainsString('4 von 4', $document->getText());
     }
 }
