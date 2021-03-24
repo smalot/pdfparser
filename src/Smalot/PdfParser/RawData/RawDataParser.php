e6bd473c0c7118684728cc99b97ec59c8db55e0d
LucianoHanna <lucianohanna@id.uff.br>
2021-03-24 14:01:53 -0300

    yoda_style

--- src/Smalot/PdfParser/RawData/RawDataParser.php
+++ src/Smalot/PdfParser/RawData/RawDataParser.php
@@ -497,7 +497,7 @@ class RawDataParser
         $offset += strspn($pdfData, '0', $offset);
         // Newer pdf may use \n instead of whitespace
         $objHeaderPattern = '/' . $objRefArr[0] . '[ \n]' . $objRefArr[1].'[ \n]obj' . '/';
-        if (preg_match($objHeaderPattern, substr($pdfData, $offset, \strlen($objHeader))) == 0) {
+        if (0 == preg_match($objHeaderPattern, substr($pdfData, $offset, \strlen($objHeader)))) {
             // an indirect reference to an undefined object shall be considered a reference to the null object
             return ['null', 'null', $offset];
         }
