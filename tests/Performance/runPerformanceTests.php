426c0628d480bd4a3aa1f8b0360cf79b3e950638
vagrant <vagrant@machine1.puphpet>
2021-07-12 12:54:25 +0000

    Testing performance test workflow

--- tests/Performance/runPerformanceTests.php
+++ tests/Performance/runPerformanceTests.php
@@ -1,5 +1,7 @@
 <?php
 
+include('vendor/autoload.php');
+
 $documentDictionaryCahceTest = new \Tests\Smalot\PdfParser\Performance\DocumentDictionaryCacheTest();
 
 $documentDictionaryCahceTest->run();
