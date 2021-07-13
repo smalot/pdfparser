bb6df215e219ef99fdb8a0b2b5cb8df715f7396d
jee7 <raimondtunnel@hotmail.com>
2021-07-13 16:29:06 +0300

    Yep, autoload does not work. Revert.

--- tests/Performance/runPerformanceTests.php
+++ tests/Performance/runPerformanceTests.php
@@ -1,6 +1,11 @@
 <?php
 
-require __DIR__.'/../../vendor/autoload.php';
+require __DIR__.'/../../alt_autoload.php-dist';
+
+// FIXME: Autoload these.
+require_once __DIR__.'/Exception/PerformanceFailException.php';
+require_once __DIR__.'/Test/AbstractPerformanceTest.php';
+require_once __DIR__.'/Test/DocumentDictionaryCacheTest.php';
 
 $tests = [
     new \Tests\Smalot\PdfParser\Performance\Test\DocumentDictionaryCacheTest(),
