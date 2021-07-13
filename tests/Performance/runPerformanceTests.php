468b0a626692695af1917bb2a96111ab8b00f9cf
jee7 <raimondtunnel@hotmail.com>
2021-07-13 19:11:46 +0300

    Performance test also succeeds, when time is exactly the same as required (although this will likely never happen).

--- tests/Performance/runPerformanceTests.php
+++ tests/Performance/runPerformanceTests.php
@@ -20,7 +20,7 @@ foreach ($tests as $test) { /* @var $test \Tests\Smalot\PdfParser\Performance\Te
 
     $time = $endTime - $startTime;
 
-    if ($test->getMaxEstimatedTime() < $time) {
+    if ($test->getMaxEstimatedTime() <= $time) {
         throw new \Tests\Smalot\PdfParser\Performance\Exception\PerformanceFailException(sprintf('Performance failed on test "%s". Time taken was %.2f seconds, expected less than %d seconds.', get_class($test), $time, $test->getMaxEstimatedTime()));
     }
 }
