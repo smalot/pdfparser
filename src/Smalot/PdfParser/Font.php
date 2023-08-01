91c1ae7c0b65ee1d61ed4d4fd0fa09d1eb9ec350
Konrad Abicht <hi@inspirito.de>
2023-08-01 09:10:18 +0200

    fixed coding style issue in Font.php

--- src/Smalot/PdfParser/Font.php
+++ src/Smalot/PdfParser/Font.php
@@ -140,7 +140,7 @@ class Font extends PDFObject
     public static function uchr($code): string
     {
         $code = (int) $code;
-        
+
         if (!isset(self::$uchrCache[$code])) {
             // html_entity_decode() will not work with UTF-16 or UTF-32 char entities,
             // therefore, we use mb_convert_encoding() instead
