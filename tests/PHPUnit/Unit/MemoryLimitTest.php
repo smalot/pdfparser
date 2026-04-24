975feb513a6c38c60aa30af27839a390316cf65c
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-24 00:58:04 -0300

    Refactor MemoryLimit test with data provider
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/PHPUnit/Unit/MemoryLimitTest.php
+++ tests/PHPUnit/Unit/MemoryLimitTest.php
@@ -3,7 +3,7 @@
 /**
  * @file This file is part of the PdfParser library.
  *
- * @author  Konrad Abicht <k.abicht@gmail.com>
+ * @author  Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
  *
  * @date    2026-04-24
  *
@@ -19,43 +19,28 @@ use Smalot\PdfParser\RawData\MemoryLimit;
 
 class MemoryLimitTest extends TestCase
 {
-    public function testToBytesWithGigabytes(): void
+    /**
+     * @dataProvider toBytesProvider
+     */
+    public function testToBytes(string $input, int $expected): void
     {
-        $this->assertSame(1073741824, MemoryLimit::toBytes('1G'));
+        $this->assertSame($expected, MemoryLimit::toBytes($input));
     }
 
-    public function testToBytesWithMegabytes(): void
+    /**
+     * @return array<string,array{0:string,1:int}>
+     */
+    public static function toBytesProvider(): array
     {
-        $this->assertSame(268435456, MemoryLimit::toBytes('256M'));
-    }
-
-    public function testToBytesWithKilobytes(): void
-    {
-        $this->assertSame(65536, MemoryLimit::toBytes('64K'));
-    }
-
-    public function testToBytesWithoutUnit(): void
-    {
-        $this->assertSame(2048, MemoryLimit::toBytes('2048'));
-    }
-
-    public function testToBytesTrimsInput(): void
-    {
-        $this->assertSame(33554432, MemoryLimit::toBytes(' 32M '));
-    }
-
-    public function testToBytesHandlesLowercaseUnits(): void
-    {
-        $this->assertSame(1048576, MemoryLimit::toBytes('1m'));
-    }
-
-    public function testToBytesReturnsMinusOneForUnlimitedValue(): void
-    {
-        $this->assertSame(-1, MemoryLimit::toBytes('-1'));
-    }
-
-    public function testToBytesReturnsMinusOneForEmptyValue(): void
-    {
-        $this->assertSame(-1, MemoryLimit::toBytes(''));
+        return [
+            'gigabytes' => ['1G', 1073741824],
+            'megabytes' => ['256M', 268435456],
+            'kilobytes' => ['64K', 65536],
+            'without unit' => ['2048', 2048],
+            'trimmed value' => [' 32M ', 33554432],
+            'lowercase unit' => ['1m', 1048576],
+            'unlimited value' => ['-1', -1],
+            'empty value' => ['', -1],
+        ];
     }
 }
