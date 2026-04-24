073e5d6642b1cf4becb21c696fe563ae6aa8d53d
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-24 00:15:17 -0300

    fix: accept multi-space xref subsection entries
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/PHPUnit/Integration/DocumentIssueFocusTest.php
+++ tests/PHPUnit/Integration/DocumentIssueFocusTest.php
@@ -111,4 +111,11 @@ class DocumentIssueFocusTest extends TestCase
         $testSubject = '•†‡…—–ƒ⁄‹›−‰„“”‘’‚™ŁŒŠŸŽıłœšž';
         self::assertStringContainsString($testSubject, $details['Subject']);
     }
+
+    public function testParseFileWithXrefSubsectionHavingMultipleSpaces(): void
+    {
+        $document = (new Parser())->parseFile($this->rootDir.'/samples/bugs/PullRequestXrefSubsectionMultipleSpaces.pdf');
+
+        self::assertCount(1, $document->getPages());
+    }
 }
